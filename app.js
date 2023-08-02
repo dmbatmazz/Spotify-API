e7d6c4bc66aa55054031e42b5ae9b194cdf16361
dmbatmazz <defnemelis8@outlook.com>
2023-08-02 17:00:51 +0300

    2 ağustos

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const axios = require('axios');
 const querystring = require('querystring');
-const mongoose = require('mongoose');
 const path = require("path");
-const app = express();
+var app = express();
 const port = 9090;
+var cors = require('cors');
+var cookieParser = require('cookie-parser');
+
 
 // Middlewares
 app.use(express.json());
@@ -14,26 +16,27 @@ app.use(express.static(path.join(__dirname, "public")));
 const clientId = 'deb4ed7d2aa24afbb0f1da4c5b8119bc'; 
 const clientSecret = '3c6c0e4459d040d2ace1bdb0b8294463'; 
 const redirectUri = 'http://localhost:9090/callback'; 
-const accessToken = 'BQDfeptYyo3SoUfKbqBtRsJvQRnxgYlJinWruOXghjeSUz8tQ1VnBWhAKCcy7MS3QOz0AVi11KWhzV9I5OKUNNjCF2BK9Y88NdEU1IX0m0OeDQgdi8o'; 
+const accessToken = 'BQDCasrH00b_NN_ke4GEks-Ac2-AcE08_u7lX0UDAKvlPP6eMSfY3Qur28XUDm2eT_A5NfLDtpzy0Bsk2e937k-cwb1Izew0u0Zs3wZ9GTWPZrvE3DAa0ERRUZCcCoFHBZY8NPlQckps-UXBY08172peICvqHe9lbPGcdsOTor_7r7GbfWeSlTkFmAi-Nm1CoUxIGVeKCviSyhaznXZUk09JoGouhGfTdR0QF_HRQI_kreYA0YjUI7ULdnO8YiF9hyA';
 
-// MongoDB bağlantısı
-const uri = "mongodb+srv://Melis:Melis123.!@cluster0.9jeu53d.mongodb.net/?retryWrites=true&w=majority"; 
+//yeni kod
+var generateRandomString = function(length) {
+  var text = '';
+  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
-mongoose.set('debug', true);
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+  for (var i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
 
-mongoose.connection.on("error", (err) => {
-  console.error("MongoDB connection error:", err);
-});
+var stateKey = 'spotify_auth_state';
 
-const userSchema = new mongoose.Schema({
-  name: String,
-  username: String,
-  playlistName: String,
-});
+app.use(express.static(__dirname + '/public'))
+   .use(cors())
+   .use(cookieParser());
 
-const User = mongoose.model("User", userSchema);
 
+//eski kodlar
 // Spotify Web API ayarları
 const spotifyAPI = axios.create({
   baseURL: "https://api.spotify.com/v1",
@@ -43,30 +46,96 @@ const spotifyAPI = axios.create({
   },
 });
 
-app.get('/login', (req, res) => {
-  const scope = 'playlist-modify-private'; // Playlist değiştirme izni artık herkes şarkı ekleyebilir
-  res.redirect(`https://accounts.spotify.com/authorize?${querystring.stringify({
-    response_type: 'code',
-    client_id: clientId,
-    scope: scope,
-    redirect_uri: redirectUri,
-  })}`);
+// app.get('/login', function(req, res) {
+
+//   var state = generateRandomString(16);
+//   var scope = 'user-read-private user-read-email';
+
+//   res.redirect('https://accounts.spotify.com/authorize?' +
+//     querystring.stringify({
+//       response_type: 'code',
+//       client_id: client_id,
+//       scope: scope,
+//       redirect_uri: redirect_uri,
+//       state: state
+//     }));
+// });
+
+//yukardaki yerine bu:
+app.get('/login', function(req, res) {
+  console.log("Melis")
+
+  var state = generateRandomString(16);
+  res.cookie(stateKey, state);
+
+  // your application requests authorization
+  var scope = 'playlist-modify-public playlist-modify-private';
+  res.redirect('https://accounts.spotify.com/authorize?' +
+    querystring.stringify({
+      response_type: 'code',
+      client_id: clientId,
+      scope: scope,
+      redirect_uri: redirectUri,
+      state: state
+    }));
 });
 
-app.get('/callback', (req, res) => {
-  const code = req.query.code || null;
-  const authOptions = {
-    url: 'https://accounts.spotify.com/api/token',
-    data: querystring.stringify({
-      code: code,
-      redirect_uri: redirectUri,
-      grant_type: 'authorization_code',
-    }),
-    headers: {
-      'Authorization': 'Basic ' + Buffer.from(`${clientId}:${clientSecret}`).toString('base64'),
-      'Content-Type': 'application/x-www-form-urlencoded',
-    },
-  };
+app.get('/callback', function(req, res) {
+
+  console.log("Meliss Callback")
+
+  var code = req.query.code || null;
+  var state = req.query.state || null;
+
+  console.log(code);
+  console.log(state)
+
+  if (state === null) {
+    res.redirect('/#' +
+      querystring.stringify({
+        error: 'state_mismatch'
+      }));
+  } else {
+    console.log("Melis Else")
+    var authOptions = {
+      url: 'https://accounts.spotify.com/api/token',
+      form: {
+        code: code,
+        redirect_uri: redirectUri,
+        grant_type: 'authorization_code'
+      },
+      headers: {
+        'Authorization': 'Basic ' + (new Buffer.from(clientId + ':' + clientSecret).toString('base64')),
+        'Content-Type' : 'application/x-www-form-urlencoded'
+      },
+      json: true
+    };
+  }
+//yeni kod
+  app.get('/refresh_token', function(req, res) {
+
+    var refresh_token = req.query.refresh_token;
+    var authOptions = {
+      url: 'https://accounts.spotify.com/api/token',
+      headers: { 'Authorization': 'Basic ' + (new Buffer.from(clientId + ':' + clientSecret).toString('base64')) },
+      form: {
+        grant_type: 'refresh_token',
+        refresh_token: refresh_token
+      },
+      json: true
+    };
+  
+    request.post(authOptions, function(error, response, body) {
+      if (!error && response.statusCode === 200) {
+        var access_token = body.access_token;
+        res.send({
+          'access_token': access_token
+        });
+      }
+    });
+  });
+  
+
 
   axios.post(authOptions.url, authOptions.data, { headers: authOptions.headers })
     .then(response => {
@@ -83,10 +152,10 @@ app.get('/callback', (req, res) => {
 
       axios.get(searchOptions.url, { headers: searchOptions.headers })
         .then(response => {
-          const trackUri = response.data.tracks.items[0].uri; // İlk seçilen şarkının URIsı
+          const trackUri = response.data.tracks.items[0].uri; 
 
           // "favorite songs" playlistine şarkı eklemek için:
-          const playlistId = '2yIwpw07FQRJgE9gTVX3zx'; // "favorite songs" playlistinin ID'si.
+          const playlistId = '3HyLpgCvdI5TLDMXLrrvrP'; // "favorite songs" playlistinin ID'si.
           const addTrackOptions = {
             url: `https://api.spotify.com/v1/playlists/${playlistId}/tracks`,
             headers: {
@@ -115,7 +184,6 @@ app.get('/callback', (req, res) => {
 
 const searchSongs = () => {
   const searchQuery = document.getElementById('searchQuery').value;
-  
   const searchOptions = {
     url: `https://api.spotify.com/v1/search?q=${encodeURIComponent(searchQuery)}&type=track`,
     headers: {
@@ -147,33 +215,39 @@ const displaySongs = (songs) => {
     // Şarkıları listeleyerek kullanıcının seçmesini
     const songElement = document.createElement('div');
     songElement.innerHTML = `<p>${songName} - ${artistName} (${albumName})</p>`;
-    songElement.addEventListener('click', () => addToFavoritePlaylist(song.uri)); // Şarkıya tıklandığında listeye ekleme
+
+    // Add butonunu oluştur
+    const addButton = document.createElement('button');
+    addButton.textContent = 'Add';
+    addButton.classList.add('add-button');
+    addButton.addEventListener('click', () => addToFavoritePlaylist(song.uri, songName, artistName));
+
+    // Şarkıyı ekrana ekle
+    songElement.appendChild(addButton);
     searchResultsDiv.appendChild(songElement);
   });
 };
 
-const addToFavoritePlaylist = (trackUri) => {
+const addToFavoritePlaylist = (trackUri, songName, artistName) => {
+  const playlistId = '3HyLpgCvdI5TLDMXLrrvrP'; // "favorite songs" playlistinin ID'si.
+
   const addTrackOptions = {
-    method: 'POST',
+    url: `https://api.spotify.com/v1/playlists/${playlistId}/tracks`,
     headers: {
       'Authorization': 'Bearer ' + accessToken,
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify({ uris: [trackUri] }),
+    data: JSON.stringify({ uris: [trackUri] }),
   };
 
-  // Şarkıyı "favorite songs" playlistine eklemek için isteği gönder
-  fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks`, addTrackOptions)
-    .then((response) => {
-      if (response.status === 201) {
-        console.log('Track added to playlist!');
-      } else {
-        console.error('Error adding track to playlist:', response.status);
-      }
+  axios.post(addTrackOptions.url, addTrackOptions.data, { headers: addTrackOptions.headers })
+    .then(() => {
+      console.log(`${songName} by ${artistName} added to playlist!`);
     })
-    .catch((error) => console.error('Error adding track to playlist:', error));
+    .catch(error => {
+      console.error('Error adding track to playlist:', error.response.status, error.response.data);
+    });
 };
-
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
